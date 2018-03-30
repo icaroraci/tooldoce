d7c1bec13214f3c9f6f8646f5f674d111fad9a45
Marcelo Mauro <marmadeoli@gmail.com>
2018-03-30 14:16:24 -0300

    Alterações requisitadas concluídas

--- src/libnfe/refNF.c
+++ src/libnfe/refNF.c
@@ -1,161 +1,173 @@
-/* TODO: Falta fazer o cabeçalho da documentação  */
+/* Copyright (c) 2017, 2018 Gabriel Lampa da Cunha <gabriellampa@gmail.com>
+ *
+ * This file is part of tooldoce.
+ *
+ * tooldoce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tooldoce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tooldoce.  If not, see <http://www.gnu.org/licenses/>.
+ * */```
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "utilidades.h"
-#include "refNF.h"
+#include "include/libnfe/defs.h"
 
 
 struct refNF_s {
-  char cUF[3];
-  char AAMM[5];
-  char CNPJ[15];
-  char mod[03];
-  char serie[4];
-  char nNF[10];
+  char cUF[TAM_CUF];
+  char AAMM[TAM_AAMM];
+  char CNPJ[TAM_CNPJ];
+  char mod[TAM_MOD_NF];
+  char serie[TAM_SERIE];
+  char nNF[TAM_NNF];
 };
 
-struct refNF_s *newRefNF()
+struct refNF_s *RefNFNew()
 {
   struct refNF_s *nf = (struct refNF_s *)malloc(sizeof(struct refNF_s));
   return nf;
 }
 
-void delRefNf(struct refNF_s *nf)
+void RefNfDel(struct refNF_s *nf)
 {
   free(nf);
 }
 
 
-void setRefNFcUF(struct refNF_s *nf, enum UF_e uf)
+void RefNFSetcUF(struct refNF_s *nf, enum UF_e uf)
 {
   strcpy(nf->cUF, ConverteUFParaCodStr(uf));
 }
 
-char *getRefNFcUF(struct refNF_s *nf)
+char *RefNFGetcUF(struct refNF_s *nf)
 {
   return nf->cUF;
 }
 
-void setRefNFAAMM(struct refNF_s *nf, const int ano, enum MES_e mes)
+void RefNFSetAAMM(struct refNF_s *nf, const int ano, enum MES_e mes)
 {
   if (ano >= 0 && ano < 100)
     strcpy(nf->AAMM, ConverteAnoParaCodStr(ano));
   strcat(nf->AAMM, ConverteMesParaCodStr(mes));
 }
 
-char *getRefNFAAMM(struct refNF_s *nf)
+char *RefNFGetAAMM(struct refNF_s *nf)
 {
   return nf->AAMM;
 }
 
-void setRefNFCNPJ(struct refNF_s *nf, const char *cnpj)
+void RefNFSetCNPJ(struct refNF_s *nf, const char *cnpj)
 {
   strcpy(nf->CNPJ, cnpj);
 }
 
-char *getRefNFCNPJ(struct refNF_s *nf)
+char *RefNFGetCNPJ(struct refNF_s *nf)
 {
   return nf->CNPJ;
 }
 
-void setRefNFMod(struct refNF_s *nf, const char *mod)
+void RefNFSetmod(struct refNF_s *nf, const char *mod)
 {
   strcpy(nf->mod, mod);
 }
 
-char *getRefNFMod(struct refNF_s *nf)
+char *RefNFGetmod(struct refNF_s *nf)
 {
   return nf->mod;
 }
 
-void setRefNFSerie(struct refNF_s *nf, const char *serie)
+void RefNFSetserie(struct refNF_s *nf, const char *serie)
 {
   strcpy(nf->serie, serie);
 }
 
-char *getRefNFSerie(struct refNF_s *nf)
+char *RefNFGetserie(struct refNF_s *nf)
 {
   return nf->serie;
 }
 
-void setRefNFnNF(struct refNF_s *nf, const char *nnf)
+void RefNFSetnNF(struct refNF_s *nf, const char *nnf)
 {
   strcpy(nf->nNF, nnf);
 }
 
-char *getRefNFnNF(struct refNF_s *nf)
+char *RefNFGetnNF(struct refNF_s *nf)
 {
   return nf->nNF;
 }
 
-/* TODO: verificar os retornos dos testes. Atualmente eles retornam
- * um objeto writer e não para a execução do programa. Isto pode
- * gerar má formação futura do xml  */
 
-xmlTextWriterPtr xmlGenRefNFNode(xmlTextWriterPtr writer, struct refNF_s *nf)
+int xmlGenRefNFNode(xmlTextWriterPtr writer, struct refNF_s *nf)
 {
   int rc;
   
   rc = xmlTextWriterStartElement(writer, BAD_CAST "NFref");
   if (rc < 0){
     printf("NFref: Erro em xmlTextWriterStartElement\n");
-    return writer;
+    return -1;
   }
   rc = xmlTextWriterStartElement(writer, BAD_CAST "refNF");
   if (rc < 0){
     printf("refNF: Erro em xmlTextWriterStartElement\n");
-    return writer;
+    return -1;
   }
   rc = xmlTextWriterWriteFormatElement(writer, BAD_CAST "cUF",
                                        "%s", getRefNFcUF(nf));
   if(rc < 0){
     printf("cUF: Erro em xmlTextWriterWriteFormatElement\n");
-    return writer;
+    return -1;
   }
   rc = xmlTextWriterWriteFormatElement(writer, BAD_CAST "AAMM",
                                        "%s", getRefNFAAMM(nf));
   if(rc < 0){
     printf("AAMM: Erro em xmlTextWriterWriteFormatElement\n");
-    return writer;
+    return -1;
   }
   rc = xmlTextWriterWriteFormatElement(writer, BAD_CAST "CNPJ",
                                        "%s", getRefNFCNPJ(nf));
   if(rc < 0){
     printf("CNPJ: Erro em xmlTextWriterWriteFormatElement\n");
-    return writer;
+    return -1;
   }
   rc = xmlTextWriterWriteFormatElement(writer, BAD_CAST "mod",
                                        "%s", getRefNFMod(nf));
   if(rc < 0){
     printf("mod: Erro em xmlTextWriterWriteFormatElement\n");
-    return writer;
+    return -1;
   }
   rc = xmlTextWriterWriteFormatElement(writer, BAD_CAST "serie",
                                        "%s", getRefNFSerie(nf));
   if(rc < 0){
     printf("serie: Erro em xmlTextWriterWriteFormatElement\n");
-    return writer;
+    return -1;
   }
   rc = xmlTextWriterWriteFormatElement(writer, BAD_CAST "nNF",
                                        "%s", getRefNFnNF(nf));
   if(rc < 0){
     printf("nNF: Erro em xmlTextWriterWriteFormatElement\n");
-    return writer;
+    return -1;
   }
   rc = xmlTextWriterEndElement(writer);
   if (rc < 0){
     printf("refNF: Erro em xmlTextWriterEndElement\n");
-    return writer;
+    return -1;
   }
   rc = xmlTextWriterEndElement(writer);
   if (rc < 0){
     printf("NFref: Erro em xmlTextWriterEndElement\n");
-    return writer;
+    return -1;
   }
 
-  return writer;
+  return 0;
 
   
   
