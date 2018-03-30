a31c336e1a6bac6bb307708c9186f9b89295d3e4
Gabriel Lampa <gabriellampa@gmail.com>
2018-03-30 12:05:50 -0300

    Commint inicial

---
+++ include/libnfe/defs.h
@@ -0,0 +1,39 @@
+/* Copyright (c) 2017, 2018 Gabriel Lampa da Cunha <gabriellampa@gmail.com>
+ *
+ * This file is part of tooldoce.
+ *
+ * tooldoce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tooldoce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tooldoce.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+
+#ifndef INCLUIDO_DEFS
+#define INCLUIDO_DEFS
+
+#define TAM_CHAVE 45
+#define TAM_CUF 3
+#define TAM_AAMM 5
+#define TAM_CNPJ 15
+#define TAM_NNF 10
+#define TAM_SERIE 4
+#define TAM_MOD_NF 3
+#define TAM_CPF 12
+#define TAM_IE 15
+#define TAM_NECF 4
+#define TAM_NCOO 7
+
+
+
+
+#endif
+
+
