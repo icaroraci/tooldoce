80c9982c2acaf61ab9bac104feaa42ed64064de6
Marcelo Mauro <marmadeoli@gmail.com>
2018-03-31 19:45:41 -0300

    modificação ide.c

--- src/libnfe/ide.c
+++ src/libnfe/ide.c
@@ -18,37 +18,40 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
-#include <time.h>
 #include "include/libnfe/defs.h"
 #include "ide.h"
 
-struct ide_c{
-  enum UF_e cUF;
-  unsigned long int cNF;           // 8 caracteres 
+struct ide_s{
+  enum UF_e cUF;                   // 2 caracteres
+  uint32_t cNF;                    // 8 caracteres 
   char natOp[TAM_NATOP];           // 61 caracteres
-  unsigned char indPag;            // 1 caractere
+  uint8_t indPag;                  // 1 caractere
   enum MOD_e mod;                  // 2 caracteres
-  unsigned short int serie;        // 3 caracteres
-  unsigned long int nNF;           // 9 caracteres
-  time_t dhEmi;                    // Data
-  time_t dhSaiEnt;                 // Data
+  uint16_t serie;                  // 3 caracteres
+  uint32_t nNF;                    // 9 caracteres
+  char *dhEmi;                     // Data
+  char *dhSaiEnt;                  // Data
   enum TIPO_NF_e tpNF;             // 1 caractere
   enum TIPO_DESTINO_e idDest;      // 1 caractere
-  unsigned int cMunFG;             // 7 caracteres
+  uint32_t cMunFG;                 // 7 caracteres
   enum TIPO_IMPRESSAO_e tpImp;     // 1 caractere
-  enum TIPO_EMISSAO_e;             // 1 caractere
-  unsigned char cDV;               // 1 caractere
+  enum TIPO_EMISSAO_e tpEmis;      // 1 caractere
+  uint8_t cDV;                     // 1 caractere
   enum TIPO_AMBIENTE_e tpAmb;      // 1 caractere
   enum TIPO_FINALIDADE_e finNFe;   // 1 caractere
   enum TIPO_OP_e indFinal;         // 1 caractere
   enum TIPO_PRES_e indPres;        // 1 caractere
   enum TIPO_PROC_EMIS_e procEmis;  // 1 caractere
   char verProc[TAM_VERSAO_APLIC];  // 20 caracteres
-  xmlTextWriterPtr writer;         // default NULL - sequencia xml;
-  time_t dhCont;                   // Data
-  char xJust[TAM_JUSTIFICATIVA];   // 256 caracteres
+  struct Cont_s *cont;             // Default NULL
 };
   
+
+struct Cont_s {
+  char *dhCont;                    // Data
+  char xJust[TAM_JUSTIFICATIVA];   // 256 caracteres
+} 
   
 }
