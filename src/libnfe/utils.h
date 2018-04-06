f4dd5ffdd0975c93116ca52e20fdb775ec5ecde2
Gabriel Lampa <gabriellampa@gmail.com>
2018-04-06 01:05:53 -0300

    Funções uteis ao projeto
    Funções de tratamento de erro, data&hora e validações

---
+++ src/libnfe/utils.h
@@ -0,0 +1,26 @@
+/* Copyright (c) 2017, 2018 Gabriel Lampa da Cunha <gabriellampa@gmail.com>
+ *
+ * This file is part of tooldoce.
+ *
+ * tooldoce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tooldoce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tooldoce.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+
+#ifndef INCLUIDO_UTILS_H
+#define INCLUIDO_UTILS_H
+
+#include<libnfe/erros.h>
+
+int error(char* msg, int codErro);
+
+#endif
